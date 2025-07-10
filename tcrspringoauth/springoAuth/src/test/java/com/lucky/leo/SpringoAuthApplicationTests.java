0f6fe837cad81e49d76800e715967bda558987cb
linkelumalai <linkelumalai@gmail.com>
2025-07-11 01:01:59 +0530

    init

---
+++ tcrspringoauth/springoAuth/src/test/java/com/lucky/leo/SpringoAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky.leo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringoAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ tcrspringoauth/springoAuthResource/src/main/java/com/lucky/leo/SpringoAuthResourceApplication.java
@@ -0,0 +1,13 @@
+package com.lucky.leo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringoAuthResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringoAuthResourceApplication.class, args);
+	}
+
+}
